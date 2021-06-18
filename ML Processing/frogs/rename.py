44d2cf2596f6abfb10f5276a4e7f618a5abb5f8c
Sophia <71236168+the-soph-ia@users.noreply.github.com>
2021-06-18 02:41:02 -0500

    Add files via upload

---
+++ ML Processing/frogs/rename.py
@@ -0,0 +1,14 @@
+# rename photo files in sequential order
+import os
+
+max = 0
+for file in os.listdir('ML Processing\\frogs\\frogs_sorted\\'):
+    f = int(file.replace('.jpg',''))
+    if f>max: max = f
+print(max)
+
+for i, name_i in enumerate(os.listdir('ML Processing\\frogs\\frogs_queue')):
+    name_f = '0'*(4-len(str(i+max)))+str(i+max)+'.jpg'
+    src = 'ML Processing\\frogs\\frogs_queue\\' + name_i
+    name_f = 'ML Processing\\frogs\\frogs_sorted\\' + name_f
+    os.rename(src,name_f)
